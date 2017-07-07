9d0077c5b18b81f4bf4143983fdad190bdd7586f
Sandi Mihaylova <sandimihaylova@gmail.com>
2017-07-06 17:12:51 -0700

    added edge variable, needs constraint on starting and ending location

--- initial_data.py
+++ initial_data.py
@@ -4,36 +4,74 @@ from random import random
 import itertools
 
 requests = pd.read_excel('data/sample_requests_min.xlsx')
+#TODO sort by requestsed at and reasssign index
+# requests = requests.sort_values('requested_at')
 contracts = pd.read_excel('data/sample_contracts_min.xlsx')
-
-# Force multiple requests per sorite
-# contracts = contracts[:1]
 flight_times = pd.read_excel('data/sample_flight_times.xlsx')
+
 
 prob = LpProblem('Tanker Assignment', LpMinimize)
 
 # Variables
-# Fake cost function
+# TODO fix cost function, minimize fuel remaining/sorties, etc
+
+# locations = list(itertools.combinations_with_replacement(list(flight_times.columns),2)
+
+base_request = list(contracts['base_id']) + list(requests['id'])
+request_request = list(requests['id'])
+request_base = list(requests['id']) + list(contracts['base_id'])
+
+destination_airspace = list(contracts['base_id']) + list(requests['airspace_id'])
+destinations = dict(zip(base_request, destination_airspace))
+
+edge_list = list(itertools.combinations(base_request,2)) +\
+list(itertools.combinations(request_request,2)) +\
+list(itertools.combinations(request_base,2))
+
+edges = []
+for edge in edge_list:
+    if edge not in edges:
+        edges.append(edge)
+
 x = []
+y = []
 cost= []
+times = []
 for contract_id in contracts['id']:
-    t= []
+    xt= []
+    yt = []
     ct = []
+    t_times = []
     for tanker_id in range(contracts['front_lines'][contract_id]):
-        r = []
+        xr = []
+        yr = []
         cr = []
+        r_times = []
         for request_id in requests['id']:
-            r.append(LpVariable("x_%s_%s:%s" % (contract_id, tanker_id, request_id), cat='Binary'))
+            xr.append(LpVariable("x_%s_%s:%s" % (contract_id, tanker_id, request_id), cat='Binary'))
             cr.append(random())
-        t.append(r)
+        for i in range(len(edges)):
+            print edges[i][0],edges[i][1]
+            yr.append(LpVariable("y_%s_%s:%s_%s" % (contract_id, tanker_id, edges[i][0], edges[i][1]), cat='Integer',lowBound=0))
+            r_times.append(flight_times[destinations[edges[i][0]]]\
+            [destinations[edges[i][1]]])
+        xt.append(xr)
+        yt.append(yr)
+        t_times.append(r_times)
         ct.append(cr)
-    x.append(t)
+    x.append(xt)
+    y.append(yt)
+    times.append(t_times)
     cost.append(ct)
+
 
 # Objective
 obj = [zip(itertools.chain(*x[i]), itertools.chain(*cost[i])) for i in range(len(x))]
+travel = [zip(itertools.chain(*y[i]), itertools.chain(*times[i])) for i in range(len(y))]
 obj_flat = [item for sublist in obj for item in sublist]
+travel_flat = [item for sublist in travel for item in sublist]
 prob += LpAffineExpression(obj_flat)
+prob += LpAffineExpression(travel_flat)
 
 #Constraints
 
@@ -44,13 +82,36 @@ for r in range(len(requests)):
     prob += lpSum([x_tanker[t][r] for t in range(len(x_tanker))]) == 1
 
 # Sortie is time feasible
-for t in x_tanker:
-    prob += LpConstraint([flight_times[requests['airspace_id'][r]][requests['airspace_id'][r-1]] - \
-    (requests['requested_at'][r] - requests['requested_at'][r-1]).seconds/60./60 \
-    for r in range(1, len(requests))] >= 0)
+# for t in x_tanker:
+#     prob += LpConstraint([flight_times[requests['airspace_id'][r]][requests['airspace_id'][r-1]] - \
+#     (requests['requested_at'][r] - requests['requested_at'][r-1]).seconds/60. \
+#     for r in range(1, len(requests))] <= 0)
 
-# Sortie is fuel feasible
 
+# Fuel feasible constraint
+# everythin but fuel burned
+for c in range(len(x)):
+    for t in range(len(x[c])):
+
+        disposable_fuel = contracts['takeoff_fuel'][c] - contracts['climbout_fuel'][c] - contracts['fuel_reserves'][c] + contracts['over_frag'][c]
+        offloaded = lpSum([x[c][t][r] * requests['amount'][r] for r in range(len(requests))])
+        # time = lpSum([x[c][t][r] * x[c][t][r-1] * flight_times[requests['airspace_id'][r-1]][requests['airspace_id'][r]] for r in range(1, len(requests))])
+        # fuel_burned = contracts['avg_burn_rate_per_hr'] * time
+        # print "t: ", t, fuel_burned
+        prob += offloaded <= disposable_fuel
+
+for c in range(len(x)):
+    for t in range(len(x[c])):
+        # for location_id in locations:
+        #     visits = lpSum([x[c][t][r] for r in range(len(requests)) if requests['airspace_id'][r] == location_id])
+        #     expected_edges = visits * 2
+        #     visited_edges = lpSum([y[c][t][i] for i in range(len(edges)) if location_id in edges[i]])
+        #     prob += expected_edges == visited_edges
+        for r in range(len(requests)):
+            inbound = lpSum([y[c][t][i] for i in range(len(edges)) if r == edges[i][0]])
+            outbound = lpSum([y[c][t][i] for i in range(len(edges)) if r == edges[i][1]])
+            prob += x[c][t][r] == inbound
+            prob += x[c][t][r] == outbound
 
 
 prob.writeLP("prob_data.lp")
